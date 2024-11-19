35766d7e2b04ef3f71f730b4c5092eba7b62370f
aaaa <aaaa@a.a>
2024-11-19 15:14:24 +0900

    시큐리 설정 레이아웃 설정 아이템 repository 메소드들 생산

---
+++ src/main/java/com/example/shoppro/dto/MemberDTO.java
@@ -0,0 +1,31 @@
+package com.example.shoppro.dto;
+
+import com.example.shoppro.constant.Role;
+import com.example.shoppro.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private Role role;
+
+
+}
+
+
